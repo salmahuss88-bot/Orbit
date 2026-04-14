d720667ad5f69f39582efef7c3581ae42ab2f472
salma <salma.huss.88@gmail.com>
2026-04-14 10:44:25 +0300

    Add basic layout: sidebar, footer and header

--- src/app/(pages)/layout.js
+++ src/app/(pages)/layout.js
@@ -1,7 +1,25 @@
+import Footer from "@/app/components/layout/Footer";
+import Header from "@/app/components/layout/Header";
+import Sidebar from "@/app/components/layout/Sidebar";
+
 export default function MainLayout({ children }) {
   return (
-    <div>
-      <main>{children}</main>
+    <div className="flex min-h-screen">
+      
+      <Sidebar />
+
+      <div className="flex-1 flex flex-col bg-[#F8FAFC]">
+        
+        <header className="p-8 pb-0">
+          <Header />
+        </header>
+
+        <main className="flex-1 p-8">
+          {children}
+        </main>
+
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/app/components/layout/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="fixed bottom-0 left-0 w-full h-16 bg-transparent flex items-center justify-center">
+        <aside className="text-center">
+          <p className="text-gray-700 text-sm font-normal">
+            Copyright © 2022 Relia Energy. All Rights Reserved
+          </p>
+        </aside>
+      </footer>
+    </>
+  );
+}
